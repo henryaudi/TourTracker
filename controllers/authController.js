1ab57d465829df799cf641f7bee437e9dcda55b4
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-13 22:46:32 -0600

    modified:   controllers/authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,13 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-  // 201 for "created"
   res.status(201).json({
     status: 'success',
     data: {
