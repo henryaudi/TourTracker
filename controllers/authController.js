5ce7cfc651e068ef39c6aa21b9b9fd58b1b1c39d
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-18 23:56:30 -0600

    modified:   controllers/authController.js 	modified:   routes/userRoutes.js 	modified:   utils/email.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -122,6 +123,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email.
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passowrdConfirm to: ${resetURL}.\nIf you didn't forget your password, just ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 minutes)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500,
+      ),
+    );
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
--- utils/email.js
+++ utils/email.js
@@ -6,9 +6,10 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    logger: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
-      passwrod: process.env.EMAIL_PASSWORD,
+      password: process.env.EMAIL_PASSWORD,
     },
   });
 
