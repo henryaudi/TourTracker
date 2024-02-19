6da3e416365a26570aed84aa6b242de974f50a06
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-18 23:58:20 -0600

    modified:   utils/email.js

--- utils/email.js
+++ utils/email.js
@@ -9,7 +9,7 @@ const sendEmail = async (options) => {
     logger: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
