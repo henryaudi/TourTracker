5965dff41be990809f0a98b64a7ee09213686c66
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-20 22:29:34 -0600

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
-
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHanlder = require('./controllers/errorController');
@@ -9,13 +14,53 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same IP.
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body.
+app.use(express.json({ limit: '10kb' })); // This only accepts data less than 10kb
+
+// Data sanitization against NoSQL query injection
+// mongoSanitize looks at req body and query string, and req.params,
+// remove all the $ sign and dots.
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// This would clean any user input from malicious HTML code.
+app.use(xss());
+
+// Prevent parameter polution.
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static file.
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
