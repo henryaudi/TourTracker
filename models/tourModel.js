4616f7070704c2d6200d957af915126b470c910a
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-24 15:34:10 -0600

    modified:   controllers/tourController.js 	modified:   models/reviewModel.js 	modified:   models/tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -122,6 +122,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() but not on .insertMant()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
