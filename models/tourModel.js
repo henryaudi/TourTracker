5f87de6fe5327a1163cfe445736f9a5874434888
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-23 21:38:45 -0600

    modified:   models/tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const validator = require('validator');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -105,7 +105,7 @@ const tourSchema = new mongoose.Schema(
         date: Number,
       },
     ],
-    guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -123,11 +123,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// OLD VERSION: EMBEDDING USER MODEL
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // QUERY MIDDLEWARE
 // the "this" keyword would now point to the query.
