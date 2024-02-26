225cfbd7ca29c55deed405ab5e1f60c0f0bd149e
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-25 23:52:42 -0600

    modified:   controllers/reviewController.js 	modified:   controllers/tourController.js 	modified:   controllers/userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,11 +78,6 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
+// DO NOT UPDATE PASSWORD WITH THIS!!!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
