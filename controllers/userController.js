3b8075f9990a62d71d165d07da90d49b5285371b
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-28 16:35:28 -0600

    modified:   controllers/userController.js 	modified:   routes/userRoutes.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
